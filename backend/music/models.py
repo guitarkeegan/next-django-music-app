b259b3e028c79583e29ea842704ba994187e784c
MAMcC0 <meredith.a.mccollum@gmail.com>
2023-01-18 20:09:26 -0800

    made message model

--- backend/music/models.py
+++ backend/music/models.py
@@ -34,3 +34,15 @@ class UserProfile(models.Model):
         choices = RoleType.choices,
         default = RoleType.Student
     )
+
+class Message(models.Model):
+    message = models.TextField(),
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    sent_at = models.DateTimeField(auto_now_add=True)
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE)
+    class Meta:
+        ordering = ['sent_at']
+
+        def __unicode__(self):
+            return self.title
+    
