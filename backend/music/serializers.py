753d704d4d208557c4e51f0768465cb69664ca9c
MAMcC0 <meredith.a.mccollum@gmail.com>
2023-01-18 20:32:45 -0800

    added seralizer for message

--- backend/music/serializers.py
+++ backend/music/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import MusicUser
+from models import UserProfile, Message
 
 class MusicUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         # fields to return?
+        fields = "__all__"
+
+class MusicMessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        # looking as well for fields to return?
         fields = "__all__"
