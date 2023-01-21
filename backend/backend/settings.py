fbfde0094be4c43dd244c7a64ecb0edc1a266816
MAMcC0 <meredith.a.mccollum@gmail.com>
2023-01-21 11:58:21 -0800

    created model for lessons and added context for media uploads, need to add apache upload limit to prevent ddos attacks

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.media"
             ],
         },
     },
@@ -104,6 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator", },
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/resources/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
--- backend/music/models.py
+++ backend/music/models.py
@@ -15,3 +15,16 @@ class Message(models.Model):
         def __unicode__(self):
             return self.title
     
+class Lesson(models.Model):
+    title= models.CharField(max_length=200, null=True)
+    description = models.TextField(null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateField(auto_now_add=True)
+    # TODO: Need to add apache limitrequest module for preventative ddos attacks 
+    lesson_video = models.FileField(upload_to= 'resources/')
+    lesson_photo = models.ImageField(upload_to='image-resources/')
+    class Meta:
+        ordering =['created_on']
+
+        def __unicode__(self):
+            return self.title
