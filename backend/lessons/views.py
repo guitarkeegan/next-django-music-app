db73fcfeaaf92744f267eaa800312ee29d5c54b2
MAMcC0 <meredith.a.mccollum@gmail.com>
2023-01-21 13:54:21 -0800

    added serializer for lesson model

--- backend/lessons/views.py
+++ backend/lessons/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Lesson
+from le
 
 # Create your views here.
