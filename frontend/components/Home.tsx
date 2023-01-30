b49321b1a262f452918a3b9843b5c5bf97b36f4f
guitarkeegan <keegananglim@gmail.com>
2023-01-29 23:11:29 -0800

    add styling to homepage

--- frontend/components/Home.tsx
+++ frontend/components/Home.tsx
@@ -3,9 +3,22 @@
 export default function Home(){
     return (
         <div>
+        <div>
             <video playsInline autoPlay muted loop poster="images/piano_background_static.png" id="bgvid" className="object-cover w-screen h-screen fixed top-0 left-0 -z-10">
                 <source src="video/home_video_background.mp4" type="video/mp4" />
            </video>
+           
         </div>
+        <div className="absolute mt-48 md:ml-8 sm:text-left py-10 px-5" style={{backgroundColor: 'rgba(104, 134, 109, .7)'}}>
+        <div className="text-white">
+            <h1 className="text-6xl">Waveform</h1>
+            <br/>
+            <br/>
+            <p className="text-2xl max-w-[300px]">A place where students and teachers and share lesson materials and schedule meetings</p>
+            <button className="bg-lighterBackground rounded text-black py-1 px-2 mt-5 focus:bg-darkerBackground hover:bg-darkerBackground">Get Started</button>
+        </div>
+       </div>
+       </div>
+        
     )
 }
