a6fde48b83919470b5b68fe1d0a528ddba39b4ec
guitarkeegan <keegananglim@gmail.com>
2023-02-02 17:44:53 -0800

    add listeners to login form

--- frontend/components/layout/Header.tsx
+++ frontend/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Header() {
     return (
         <div className="absolute text-white w-screen h-36 flex justify-between items-center px-20 text-lg" style={{backgroundColor: 'rgba(104, 134, 109, .7)'}}>
-            <p className=''>Home</p>
+            <Link href="/" className=''>Home</Link>
             <Link href='/another-page'>Another Page</Link>
         </div>
     )
