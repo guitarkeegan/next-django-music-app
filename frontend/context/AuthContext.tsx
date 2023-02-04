aaedbca7f6b79aedecf157dcfd24b769e2bf4f9c
guitarkeegan <keegananglim@gmail.com>
2023-02-04 13:54:46 -0800

    refactoring usecontext

---
+++ frontend/context/AuthContext.tsx
@@ -0,0 +1,49 @@
+import {createContext, useContext, useState} from 'react';
+import type { ReactNode } from 'react';
+
+type authContextType = {
+    user: null | boolean;
+    login: () => void;
+    logout: () => void;
+}
+
+const authContextDefaultValues: authContextType = {
+    user: null,
+    login: () => {},
+    logout: () => {},
+}
+
+const AuthContext = createContext<authContextType>(authContextDefaultValues);
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
+
+type Props = {
+    children: ReactNode;
+}
+
+export function AuthProvider({children}: Props){
+    const [user, setUser] = useState<boolean | null>(null);
+
+    const login = () => {
+        setUser(true);
+    }
+
+    const logout = () => {
+        setUser(false);
+    }
+
+    const value ={
+        user,
+        login,
+        logout
+    }
+    return (
+        <>
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+        </>
+    )
+}
